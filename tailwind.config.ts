7bcce59cad03adc5e761e44a5b60244468addb69
Alex Foster <xrk4np@virginia.edu>
2023-09-01 12:10:50 -0400

    extend grid templates in tailwind cfg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "16": "repeat(16, minmax(0, 1fr))",
+        "17": "repeat(17, minmax(0, 1fr))",
+      },
+      gridTemplateRows: {
+        "16": "repeat(16, minmax(0, 1fr))",
+        "17": "repeat(17, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
